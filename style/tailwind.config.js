9de7fe21b1ef62d06707244f970c09dce17d654b
alm3ndr1t <almendrit03@icloud.com>
2024-03-12 18:29:52 +0100

    initial commit

---
+++ style/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  darkMode: "class",
+  // You can add any file here to be read by tailwind
+  content: ["src/presentation/templates/**/*.html", "scripts/*.js"],
+};
